c073e2d98f8e7f4af4b30cc0b96f33053abee5d2
himankmathur18 <himankmathur9@gmail.com>
2025-06-05 20:10:47 +0530

    update 6

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -11,19 +11,25 @@ function Experience() {
   ];
 
   return (
-    <section id="experience" className="py-10 h-screen">
-        <h2 className="text-3xl font-bold text-white bg-blue-950 border-b-1 p-4 mt-[1.5em]">Experience</h2>
-        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2">
-          {experiences.map((exp, index) => (
-            <div key={index} className="bg-white rounded-lg shadow-md p-4 m-10 hover:p-5 duration-300">
-              <h3 className="text-xl font-semibold">{exp.title}</h3>
-              <h4 className="text-lg text-gray-600">{exp.company}</h4>
-              <p className="text-gray-500 mb-2">{exp.duration}</p>
-            
-            </div>
-          ))}
-        </div>
-      
+    <section
+      id="experience"
+      className="py-6 min-h-[60vh] sm:py-10 sm:min-h-screen"
+    >
+      <h2 className="text-3xl font-bold text-white bg-blue-950 border-b-1 p-4 mt-[1.5em]">
+        Experience
+      </h2>
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2">
+        {experiences.map((exp, index) => (
+          <div
+            key={index}
+            className="bg-white rounded-lg shadow-md p-3 xs:p-4 m-4 xs:m-6 sm:m-10 hover:p-4 xs:hover:p-5 duration-300"
+          >
+            <h3 className="text-base xs:text-lg sm:text-xl font-semibold">{exp.title}</h3>
+            <h4 className="text-sm xs:text-base sm:text-lg text-gray-600">{exp.company}</h4>
+            <p className="text-xs xs:text-sm sm:text-base text-gray-500 mb-2">{exp.duration}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
